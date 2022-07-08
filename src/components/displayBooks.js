4839e81c6b3348bc1aaf9506ef916ad8943303fc
safar ali <miqra019@gmail.com>
2022-07-08 13:21:47 +0500

    solve linters

--- src/components/displayBooks.js
+++ src/components/displayBooks.js
@@ -3,16 +3,14 @@ import AddBook from './form';
 import SingleBook from './singleBook';
 
 const DisplayBooks = () => {
-    const books = [];
+  const books = [];
 
-    return (
-  <div className="book-list">
-       {books.map((book) => <SingleBook title={book.title} author={book.author} />)}
-       <AddBook />
-  </div>
-)};
+  return (
+    <div className="book-list">
+      {books.map((book) => <SingleBook key={1} title={book.title} author={book.author} />)}
+      <AddBook />
+    </div>
+  );
+};
 
 export default DisplayBooks;
-
-
-
